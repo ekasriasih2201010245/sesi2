485a0401d7590056828bf9b5dc77a7e35f5d2061
ekasriasih2201010245 <127836733+ekasriasih2201010245@users.noreply.github.com>
2023-03-25 09:34:32 +0800

    upload project

---
+++ latihan02.php
@@ -0,0 +1,12 @@
+<?php
+    $angka = 3;
+
+    echo "nilai angka: <strong>". $angka."</strong>";
+
+    if($angka > 4){
+        //kondisi true
+        echo "<br>nilai angka lebih dari 4";
+    }else{
+        //kondisi fals
+        echo "<br>nilai angka kurang dari 4";
+    }
